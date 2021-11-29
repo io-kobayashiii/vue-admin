ce3e3dfd0d50a37d95b5b382c98485f28bb6513f
io-kobayashiii <io.kobayashiii@gmail.com>
2021-11-29 13:34:33 +0900

    watch tailwindcss in jit mode

--- server/resources/views/index.blade.php
+++ server/resources/views/index.blade.php
@@ -1,11 +1,11 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Guest</title>
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
