ae72f62b6500bd861174d2f0b4050cb60b9e96b3
io-kobayashiii <io.kobayashiii@gmail.com>
2021-12-01 03:03:00 +0900

    add default layout blade / close #12

--- server/resources/views/index.blade.php
+++ server/resources/views/index.blade.php
@@ -1,19 +1,6 @@
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Guest</title>
-    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body>
-    <div id="app">
-        <h1>Hello World</h1>
-        <example-component></example-component>
-    </div>
-</body>
-<script src="{{ mix('js/app.js') }}"></script>
-
-</html>
+@extends('layouts.default')
+@section('pageTitle', 'top | admin-panel')
+@section('bodyClasses', 'page-top')
+@section('pageContents')
+    <example-component />
+@endsection
