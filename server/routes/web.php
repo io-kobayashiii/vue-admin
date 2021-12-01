398ac9031b14a5d955e0edb012a86c05356e4784
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-12-01 23:14:24 +0900

    fix

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'Test\IndexController@index')->name('index');
+Route::get('/{any}', 'Test\IndexController@index')
+    ->name('index')
+    ->where('any', '.*');
